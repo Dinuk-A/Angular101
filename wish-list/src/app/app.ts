897bbe0476cfdb494322262249765cb0503f40ec
Dinuka Pramod <dinukah98@gmail.com>
2025-11-05 18:42:24 +0530

    installed bootstrap and notes added

--- wish-list/src/app/app.ts
+++ wish-list/src/app/app.ts
@@ -1,5 +1,6 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { wishItem } from './shared/models/wishItem';
 
 @Component({
   selector: 'app-root',
@@ -8,5 +9,12 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
+
+  items = [
+    new wishItem('Learn Angular'),
+    new wishItem('Drink coffee', true),
+    new wishItem('Go to mars')
+  ]
+
   protected readonly title = signal('wish-list');
 }
---
+++ wish-list/src/app/shared/models/wishItem.ts
@@ -0,0 +1,11 @@
+export class wishItem {
+
+    //constructor is a TS feature,this will auto create properties for this class
+     //false is isDone's default value
+    constructor(public wishText: string, public isDone: boolean = false) {
+
+    }
+
+   
+
+}
