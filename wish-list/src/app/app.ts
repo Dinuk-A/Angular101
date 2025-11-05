aa576654cbec14132156bb6c4cc06d9b2d22212f
Dinuka Pramod <dinukah98@gmail.com>
2025-11-05 19:18:29 +0530

    ngFor lesson added

--- wish-list/src/app/app.ts
+++ wish-list/src/app/app.ts
@@ -1,15 +1,17 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { wishItem } from './shared/models/wishItem';
+import { CommonModule } from '@angular/common'; 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [CommonModule,RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
 
+  // Create an array of wishItem objects
   items = [
     new wishItem('Learn Angular'),
     new wishItem('Drink coffee', true),
