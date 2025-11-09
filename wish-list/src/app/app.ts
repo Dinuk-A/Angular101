2b1f54935a8e7c85a3c932f8130c980fe051acab
Dinuka Pramod <dinukah98@gmail.com>
2025-11-09 16:57:51 +0530

    ngmodel lesson added

--- wish-list/src/app/app.ts
+++ wish-list/src/app/app.ts
@@ -2,23 +2,15 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { wishItem } from './shared/models/wishItem';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, FormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-
-  //e here is from event, its optional, just to show lesson
-  toggleItem(e:any, wish:wishItem) {
-   console.log("clicked ", e);
-   wish.isDone = !wish.isDone;
-   console.log(wish);
-   
-   
-  }
 
   // Create an array of wishItem objects
   // first version, without any type
@@ -36,4 +28,33 @@ export class App {
   ]
 
   protected readonly title = signal('wish-list');
+
+   // a new property
+   newWishText = '';
+
+  //add a new wish to array
+  addNewWish() {
+
+    //'this' refers to the class App
+    //'this.items' = the array of wishItem objects
+    // 'this.newWishText' = property that holds the text from input box
+
+    //by default the boolean is false
+    this.items.push(new wishItem(this.newWishText));
+
+    //clear out the value of property
+    this.newWishText = '';
+    
+  }
+
+  //e here is from event, its optional, just to show lesson
+  toggleItem(e: any, wish: wishItem) {
+    console.log("clicked ", e);
+    wish.isDone = !wish.isDone;
+    console.log(wish);
+  }
+
+
+
+
 }
